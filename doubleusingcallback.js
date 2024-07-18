add5b8d7d218f5c19645799bdbd57ca2bc9b74e3
aman2003-coder <122880804+aman2003-coder@users.noreply.github.com>
2024-07-19 00:14:51 +0530

    Add files via upload

---
+++ doubleusingcallback.js
@@ -0,0 +1,16 @@
+function doubleArray(arr, callback) {
+    const doubledArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      const doubled = callback(arr[i]);
+      doubledArr.push(doubled);
+    }
+    return doubledArr;
+  }
+  const arr = [1, 2, 3, 4, 5];
+
+function double(num) {
+  return num * 2;
+}
+
+const doubledArr = doubleArray(arr, double);
+console.log(doubledArr);
